e35fc04b5c15eaa0abcf1cdd1f696c841dfc0699
Darkhan Tashim <200103208@stu.sdu.edu.kz>
2022-10-26 15:19:57 +0600

    added layouts

---
+++ app/src/main/java/com/example/universalapp/CountdownTimerForWorkoutActivity.kt
@@ -0,0 +1,11 @@
+package com.example.universalapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CountdownTimerForWorkoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_countdown_timer_for_workout)
+    }
+}
