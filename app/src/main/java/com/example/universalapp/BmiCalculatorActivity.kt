a49009da6c19120e265257b44d6dc5f61d9808fa
Darkhan Tashim <200103208@stu.sdu.edu.kz>
2022-10-31 22:06:03 +0600

    added some design to bmi activity2

--- app/src/main/java/com/example/universalapp/BmiCalculatorActivity.kt
+++ app/src/main/java/com/example/universalapp/BmiCalculatorActivity.kt
@@ -1,11 +1,54 @@
 package com.example.universalapp
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class BmiCalculatorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bmi_calculator)
+
+        val calculateBtn: Button = findViewById(R.id.btn_calculate)
+        val height: EditText = findViewById(R.id.height)
+        val weight: EditText = findViewById(R.id.weight)
+        val result: TextView = findViewById(R.id.result)
+
+        calculateBtn.setOnClickListener {
+            if (height == null || weight == null) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                val h = height.text.toString().toFloat() / 100 // /100 to get height in meter
+                val w = weight.text.toString().toFloat()
+                val res = w / (h * h) // formula to calculate the BMI\
+                if (res <= 18.5) {
+                    result.text = "%.1f".format(res).plus(" BMI").plus("\n Underweight")
+                    result.setTextColor(Color.parseColor("#469eeb"))
+                } else if (res > 18.5 && res <= 25.0) {
+                    result.text = "%.1f".format(res).plus(" BMI").plus("\n Normal")
+                    result.setTextColor(Color.parseColor("#02a121"))
+                } else {
+                    result.text = "%.1f".format(res).plus(" BMI").plus("\n Overweight")
+                    result.setTextColor(Color.parseColor("#ff425f"))
+                }
+
+                hideSoftKeyboard(calculateBtn)
+            }
+        }
+    }
+
+    private fun hideSoftKeyboard(view: View) {
+        val imm =
+            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
--- app/src/main/java/com/example/universalapp/MainActivity.kt
+++ app/src/main/java/com/example/universalapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.universalapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn : Button = findViewById(R.id.bmi_calculator)
+        btn.setOnClickListener {
+            openBmiCalculatorActivity()
+        }
+    }
+
+    private fun openBmiCalculatorActivity() {
+        val intent = Intent(this, BmiCalculatorActivity::class.java)
+        startActivity(intent)
     }
 }
