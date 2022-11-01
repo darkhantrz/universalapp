26ced2a44a97317b6d844eadb6708cd8ed717d89
Darkhan Tashim <200103208@stu.sdu.edu.kz>
2022-11-01 23:57:39 +0600

    added nav drawer

--- app/src/main/java/com/example/universalapp/BmiCalculatorActivity.kt
+++ app/src/main/java/com/example/universalapp/BmiCalculatorActivity.kt
@@ -1,6 +1,8 @@
 package com.example.universalapp
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,10 +10,12 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class BmiCalculatorActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bmi_calculator)
@@ -48,6 +52,12 @@ class BmiCalculatorActivity : AppCompatActivity() {
 
                 hideSoftKeyboard(calculateBtn) // to close keyboard after tapping the button
             }
+        }
+
+        val goBackToMainActivity: ImageView = findViewById(R.id.goBackToMain)
+
+        goBackToMainActivity.setOnClickListener {
+            finish()
         }
     }
 
--- app/src/main/java/com/example/universalapp/MainActivity.kt
+++ app/src/main/java/com/example/universalapp/MainActivity.kt
@@ -4,19 +4,41 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.cardview.widget.CardView
 import com.example.universalapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var toggle: ActionBarDrawerToggle
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.apply {
+            toggle = ActionBarDrawerToggle(this@MainActivity, drawerLayout, R.string.open, R.string.close)
+            drawerLayout.addDrawerListener(toggle)
+            toggle.syncState()
+
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+            navView.setNavigationItemSelectedListener {
+                when (it.itemId) {
+                    R.id.bmi -> { openBmiCalculatorActivity() }
+                    R.id.countdown -> {}
+                    R.id.weather -> {}
+                    R.id.alarm -> {}
+                }
+                true
+            }
+
+        }
 
         val bmiCalculatorCardView : CardView = binding.cardView1
         bmiCalculatorCardView.setOnClickListener {
@@ -24,8 +46,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun openBmiCalculatorActivity() {
         val intent = Intent(this, BmiCalculatorActivity::class.java)
         startActivity(intent)
+        binding.drawerLayout.close()
     }
 }
